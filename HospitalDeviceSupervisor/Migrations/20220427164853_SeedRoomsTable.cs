90995f46e9d02720ffd6c9ddc4927ec8dda1f3ca
Lev <nedra.tailog@gmail.com>
2022-04-27 20:01:23 +0300

    Were seeded Rooms table and Locations table

---
+++ HospitalDeviceSupervisor/Migrations/20220427164853_SeedRoomsTable.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospitalDeviceSupervisor.Migrations
+{
+    public partial class SeedRoomsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string sql = (@"
+                SET IDENTITY_INSERT Rooms ON
+                Insert INTO Rooms (Id, Name) Values (1, 12)
+                Insert INTO Rooms(Id, Name) Values(2, '13')
+                Insert INTO Rooms(Id, Name) Values(3, '14')
+                Insert INTO Rooms(Id, Name) Values(4, '15')
+                Insert INTO Rooms(Id, Name) Values(5, '16')
+                Insert INTO Rooms(Id, Name) Values(6, '17')
+                Insert INTO Rooms(Id, Name) Values(7, '18')
+                Insert INTO Rooms(Id, Name) Values(8, '19')
+                Insert INTO Rooms(Id, Name) Values(9, '20')
+                Insert INTO Rooms(Id, Name) Values(10, '21')
+                Insert INTO Rooms(Id, Name) Values(11, '22')
+                Insert INTO Rooms(Id, Name) Values(12, '23')
+                Insert INTO Rooms(Id, Name) Values(13, '24')
+                Insert INTO Rooms(Id, Name) Values(14, '25')
+                Insert INTO Rooms(Id, Name) Values(15, '26')
+                Insert INTO Rooms(Id, Name) Values(16, '27')
+                Insert INTO Rooms(Id, Name) Values(17, '30')
+                Insert INTO Rooms(Id, Name) Values(18, 'Экспресс лаборатория')
+                SET IDENTITY_INSERT Rooms OFF
+            ");
+
+            migrationBuilder.Sql(sql);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            string sql = (@"
+                Delete Rooms Where Id = 1
+                Delete Rooms Where Id = 2
+                Delete Rooms Where Id = 3
+                Delete Rooms Where Id = 4
+                Delete Rooms Where Id = 5
+                Delete Rooms Where Id = 6
+                Delete Rooms Where Id = 7
+                Delete Rooms Where Id = 8
+                Delete Rooms Where Id = 9
+                Delete Rooms Where Id = 10
+                Delete Rooms Where Id = 11
+                Delete Rooms Where Id = 12
+                Delete Rooms Where Id = 13
+                Delete Rooms Where Id = 14
+                Delete Rooms Where Id = 15
+                Delete Rooms Where Id = 16
+                Delete Rooms Where Id = 17
+                Delete Rooms Where Id = 18
+            ");
+
+            migrationBuilder.Sql(sql);
+        }
+    }
+}
---
+++ HospitalDeviceSupervisor/Migrations/20220427165403_SeedLocationsTable.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospitalDeviceSupervisor.Migrations
+{
+    public partial class SeedLocationsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string sql = (@"
+                SET IDENTITY_INSERT Locations ON
+                Insert INTO Locations (Id, BuildingId, RoomId) Values (1, 1, 1)
+                Insert INTO Locations (Id, BuildingId, RoomId) Values (2, 1, 2)
+                Insert INTO Locations (Id, BuildingId, RoomId) Values (3, 1, 3)
+                Insert INTO Locations (Id, BuildingId, RoomId) Values (4, 1, 4)
+                Insert INTO Locations (Id, BuildingId, RoomId) Values (5, 1, 5)
+                Insert INTO Locations (Id, BuildingId, RoomId) Values (6, 1, 6)
+                Insert INTO Locations (Id, BuildingId, RoomId) Values (7, 1, 7)
+                Insert INTO Locations (Id, BuildingId, RoomId) Values (8, 1, 8)
+                Insert INTO Locations (Id, BuildingId, RoomId) Values (9, 1, 9)
+                Insert INTO Locations (Id, BuildingId, RoomId) Values (10, 1, 10)
+                Insert INTO Locations (Id, BuildingId, RoomId) Values (11, 1, 11)
+                Insert INTO Locations (Id, BuildingId, RoomId) Values (12, 1, 12)
+                Insert INTO Locations (Id, BuildingId, RoomId) Values (13, 1, 13)
+                Insert INTO Locations (Id, BuildingId, RoomId) Values (14, 1, 14)
+                Insert INTO Locations (Id, BuildingId, RoomId) Values (15, 1, 15)
+                Insert INTO Locations (Id, BuildingId, RoomId) Values (16, 1, 16)
+                Insert INTO Locations (Id, BuildingId, RoomId) Values (17, 1, 17)
+                Insert INTO Locations (Id, BuildingId, RoomId) Values (18, 1, 18)
+                SET IDENTITY_INSERT Locations OFF
+            ");
+
+            migrationBuilder.Sql(sql);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            string sql = (@"
+                Delete Locations Where Id = 1
+                Delete Locations Where Id = 2
+                Delete Locations Where Id = 3
+                Delete Locations Where Id = 4
+                Delete Locations Where Id = 5
+                Delete Locations Where Id = 6
+                Delete Locations Where Id = 7
+                Delete Locations Where Id = 8
+                Delete Locations Where Id = 9
+                Delete Locations Where Id = 10
+                Delete Locations Where Id = 11
+                Delete Locations Where Id = 12
+                Delete Locations Where Id = 13
+                Delete Locations Where Id = 14
+                Delete Locations Where Id = 15
+                Delete Locations Where Id = 16
+                Delete Locations Where Id = 17
+                Delete Locations Where Id = 18
+            ");
+
+            migrationBuilder.Sql(sql);
+        }
+    }
+}
